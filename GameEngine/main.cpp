4bffb51abd40deeb0d303a879ef99252785d45a2
David Chadwick <dave@king.com>
2017-02-23 10:37:35 +0000

    setting the new config

--- GameEngine/main.cpp
+++ GameEngine/main.cpp
@@ -1,13 +1,13 @@
 #include "src\graphics\window.h"
 
 // This is a standard 2d/3d game engine with a primary focus on mobile application developments
-// Created by Dave Chadwick 2017
+// Created by Dave Chadwick Feb 2017
 
 int main() {
 
 	using namespace DCEngine;
 	using namespace graphics;
-	Window window("Dave!", 800, 600);
+	Window window("Dave!", 960, 540);
 	glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
 
 	GLuint vao;
@@ -18,16 +18,17 @@ int main() {
 	{
 		window.clear();
 
-#if 0
-
+#if 1
 		glBegin(GL_QUADS);
 		glVertex2f(-0.5f, -0.5f);
 		glVertex2f(-0.5f,  0.5f);
 		glVertex2f( 0.5f,  0.5f);
 		glVertex2f( 0.5f, -0.5f);
 		glEnd();
-#endif
+#else
 		glDrawArrays(GL_ARRAY_BUFFER, 0, 0);
+#endif
+		
 		window.update();
 	}
 
