f59a9e3a7864f329d429ad185d499bd0a58bf915
David Chadwick <dave@king.com>
2017-03-03 16:29:54 +0000

    we now have a simple 2 renderer

--- GameEngine/main.cpp
+++ GameEngine/main.cpp
@@ -6,6 +6,9 @@
 #include "src\graphics\buffers\indexbuffer.h"
 #include "src\graphics\buffers\vertexarray.h"
 
+#include "src/graphics/renderer2d.h"
+#include "src\graphics\simple2drenderer.h"
+
 #define BUFFER_OFFSET(i) ((char *)NULL + (i))
 
 // This is a standard 2d/3d game engine with a primary focus on mobile application developments
@@ -20,100 +23,29 @@ int main() {
 	Window window("DC Engine!", 960, 540);
 	//glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
-#if 0
-
-	GLfloat verticies[] = {
-		0, 0, 0,
-		8, 0, 0,
-		0, 3, 0,
-		0, 3, 0,
-		8, 3, 0,
-		8, 0, 0
-	};
-
-	GLuint vertex_buffer;
-	glGenBuffers(1, &vertex_buffer);
-	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glEnableVertexAttribArray(0);
-
-
-#else
-
-	GLfloat verticies[] = {
-		0, 0, 0,
-		0, 3, 0,
-		8, 3, 0,
-		8, 0, 0
-	};
-
-	GLushort indecies[] = {
-		0, 1, 2,
-		2, 3, 0
-	};
-
-	GLfloat coloursA[] = {
-		1, 0, 1, 1,
-		1, 0, 1, 1,
-		1, 0, 1, 1,
-		1, 0, 1, 1
-	};
-
-	GLfloat coloursB[] = {
-		0.2f, 0.3f, 0.8f, 1,
-		0.2f, 0.3f, 0.8f, 1,
-		0.2f, 0.3f, 0.8f, 1,
-		0.2f, 0.3f, 0.8f, 1
-	};
-
-	VertexArray sprite1, sprite2;
-	IndexBuffer ibo(indecies, 6);
-
-	sprite1.addBuffer(new Buffer(verticies, 4 * 3, 3), 0);
-	sprite1.addBuffer(new Buffer(coloursA, 4 * 4, 4), 1);
-	sprite2.addBuffer(new Buffer(verticies, 4 * 3, 3), 0);
-	sprite2.addBuffer(new Buffer(coloursB, 4 * 4, 4), 1);
-
-#endif
-
 	mat4 ortho = mat4::orthographic(0.0f, 16.0f, 0.0f, 9.0f, -1.0f, 1.0f);
 
 	Shader shader("src/shaders/basic.vert", "src/shaders/basic.frag");
 	shader.enable();
 	shader.setUniformMat4("pr_matrix", ortho);
 	shader.setUniformMat4("ml_matrix", mat4::translation(vec3(4, 3, 0)));
-
 	shader.setUniform2f("light_pos", vec2(8.0f, 4.5f));
 	shader.setUniform4f("color", vec4(0.2f, 0.3f, 0.8f, 1.0f));
-
 	shader.listUniforms();
 	shader.listAttributes();
+
+	Renderable2D sprite(maths::vec3(5, 5, 0), maths::vec2(4, 4), maths::vec4(1, 0, 1, 1), shader);
+	Renderable2D sprite2(maths::vec3(7, 1, 0), maths::vec2(2, 3), maths::vec4(0.2f, 0, 1, 1), shader);
+	Simple2DRenderer renderer;
 
 	while (!window.closed()){
 		window.clear();
 		double x, y;
 		window.getMousePosition(x, y);
 		shader.setUniform2f("light_pos", vec2((float)(x * 16.0f / 960.0f), (float)(9.0f - y * 9.0f / 540.0f)));
-
-#if 0
-		glDrawArrays(GL_TRIANGLES, 0, 6);
-#else
-		sprite1.bind();
-		ibo.bind();
-		shader.setUniformMat4("ml_matrix", mat4::translation(vec3(4, 3, 0)));
-		glDrawElements(GL_TRIANGLES, ibo.getCount(), GL_UNSIGNED_SHORT, BUFFER_OFFSET(0));
-		ibo.bind();
-		sprite1.unbind();
-
-		sprite2.bind();
-		ibo.bind();
-		shader.setUniformMat4("ml_matrix", mat4::translation(vec3(0, 0, 0)));
-		glDrawElements(GL_TRIANGLES, ibo.getCount(), GL_UNSIGNED_SHORT, BUFFER_OFFSET(0));
-		ibo.bind();
-		sprite2.unbind();
-#endif
-
+		renderer.submit(&sprite);
+		renderer.submit(&sprite2);
+		renderer.flush();
 		window.update();
 	}
 
